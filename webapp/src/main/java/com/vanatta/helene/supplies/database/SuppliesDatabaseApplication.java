706cb14e765354eb72c0efb2c71e2215b7079d6f
DanVanAtta <dhvatta@gmail.com>
2024-11-08 23:07:34 -0500

    Add boilerplate springboot files

---
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/SuppliesDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.vanatta.helene.supplies.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuppliesDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuppliesDatabaseApplication.class, args);
+	}
+
+}
---
+++ webapp/src/test/java/com/vanatta/helene/supplies/database/SuppliesDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanatta.helene.supplies.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuppliesDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
