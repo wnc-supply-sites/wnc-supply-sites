d382861bcfb27052a67c8b98cc7dbdbe7e214dae
DanVanAtta <dhvatta@gmail.com>
2024-11-11 11:14:10 -0500

    DB connection pooling, use HikariCP

--- webapp/src/main/java/com/vanatta/helene/supplies/database/JdbiConfiguration.java
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/JdbiConfiguration.java
@@ -1,5 +1,7 @@
 package com.vanatta.helene.supplies.database;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
@@ -17,6 +19,21 @@ public class JdbiConfiguration {
       @Value("${jdbi.user}") String user,
       @Value("${jdbi.password}") String password) {
     log.info("Using DB url: {}, user: {}", url, user);
-    return Jdbi.create(url, user, password).installPlugin(new SqlObjectPlugin());
+
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl(url);
+    config.setUsername(user);
+    config.setPassword(password);
+
+    // config values docs: https://github.com/brettwooldridge/HikariCP?tab=readme-ov-file#gear-configuration-knobs-baby
+
+    // timeout DB connections at 10s instead of default of 30s
+    config.addDataSourceProperty("connectionTimeout", 10_000);
+
+    // Keep pool size small (default is 10). Target env is a single core linode.
+    // https://github.com/brettwooldridge/HikariCP/wiki/About-Pool-Sizing
+    config.addDataSourceProperty("maximumPoolSize", "4");
+    HikariDataSource ds = new HikariDataSource(config);
+    return Jdbi.create(ds).installPlugin(new SqlObjectPlugin());
   }
 }
