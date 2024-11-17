871c7712d29af84c9663af1c52af0c2ba89c2740
DanVanAtta <dhvatta@gmail.com>
2024-11-17 17:27:20 -0500

    Javascript updates for inventory page

--- webapp/src/main/resources/public/manage/inventory.js
+++ webapp/src/main/resources/public/manage/inventory.js
@@ -25,8 +25,10 @@ function toggleInventory(itemName) {
       labelClass = "oversupply";
     }
 
-    document.getElementById(`${itemName}Label`)
-    .classList = "larger " + labelClass;
+    document.getElementById(`${itemName}Label`).classList.value = "larger " + labelClass;
+    // .classList.add("larger");
+    // document.getElementById(`${itemName}Label`)
+    // .classList.add(labelClass);
 
     document.getElementById(`${itemName}RequestedLabel`)
     .classList.remove("disabledInventory");
@@ -35,8 +37,12 @@ function toggleInventory(itemName) {
     document.getElementById(`${itemName}OversupplyLabel`)
     .classList.remove("disabledInventory");
   } else {
-    document.getElementById(`${itemName}Label`)
-        .classList = "larger disabledInventory";
+    document.getElementById(`${itemName}Label`).classList.value = "larger disabledInventory";
+
+    // document.getElementById(`${itemName}Label`)
+    // .classList.add("larger");
+    // document.getElementById(`${itemName}Label`)
+    // .classList.add("disabledInventory");
 
     document.getElementById(`${itemName}RequestedLabel`)
     .classList.add("disabledInventory");
@@ -87,28 +93,66 @@ function addItem() {
   const oversupplyChecked = document.getElementById("oversupplyRadioNew")
       .checked ? "checked" : "";
 
+  let labelStyle = "";
+  if (requestedChecked) {
+    labelStyle = "requested";
+  } else if (urgentChecked) {
+    labelStyle = "urgent";
+  } else {
+    labelStyle = "oversupply";
+  }
 
   const newItemRowHtml = `
-          <tr>
+        <tr>
+          <!-- Item Checkbox -->
           <td><input type="checkbox" id="${itemNameEncoded}Checkbox"
-                     onclick="toggleInventory('${itemNameEncoded}')" checked></input></td>
-          <td><label for="${itemNameEncoded}Checkbox" class="larger">${itemNameEncoded}</label></td>
+                     onclick="toggleInventory('${itemNameEncoded}')" checked /></td>
+                     
+           <!-- Item Label -->
+          <td><label for="${itemNameEncoded}Checkbox" class="larger ${labelStyle}" id="${itemNameEncoded}Label">${itemNameEncoded}</label></td>
+          <!-- Item Status Radio buttons -->
           <td>
             <div class="horizontal">
+            
               <fieldset class="vertical">
                 <div class="horizontal">
-                  <input type="radio" id="${itemNameEncoded}Requested" name="${itemNameEncoded}Status" ${requestedChecked}/>
-                  <label for="${itemNameEncoded}Requested" class="requested" id="${itemNameEncoded}RequestedLabel">Requested or
-                    Available</label>
+                  <input 
+                      type="radio" 
+                      id="${itemNameEncoded}Requested" 
+                      name="${itemNameEncoded}Status" 
+                      onclick="changeItemStatus('${itemNameEncoded}')"
+                      ${requestedChecked}/>
+                  <label 
+                      for="${itemNameEncoded}Requested" 
+                      class="requested" 
+                      id="${itemNameEncoded}RequestedLabel">
+                    Requested or Available</label>
                 </div>
                 <div class="horizontal">
-                  <input type="radio" id="${itemNameEncoded}Urgent" name="${itemNameEncoded}Status" ${urgentChecked}/>
-                  <label for="${itemNameEncoded}Urgent" class="urgent" id="${itemNameEncoded}UrgentLabel">Urgently Needed</label>
+                  <input 
+                      type="radio" 
+                      id="${itemNameEncoded}Urgent" 
+                      name="${itemNameEncoded}Status" 
+                      onclick="changeItemStatus('${itemNameEncoded}')"
+                      ${urgentChecked}/>
+                  <label 
+                      for="${itemNameEncoded}Urgent" 
+                      class="urgent" 
+                      id="${itemNameEncoded}UrgentLabel">
+                    Urgently Needed</label>
                 </div>
                 <div class="horizontal">
-                  <input type="radio" id="${itemNameEncoded}Oversupply" name="${itemNameEncoded}Status" ${oversupplyChecked}/>
-                  <label for="${itemNameEncoded}Oversupply" class="oversupply" id="${itemNameEncoded}OversupplyLabel">Oversupply (too
-                    much)</label>
+                  <input 
+                      type="radio" 
+                      id="${itemNameEncoded}Oversupply" 
+                      name="${itemNameEncoded}Status" 
+                      onclick="changeItemStatus('${itemNameEncoded}')"
+                      ${oversupplyChecked}/>
+                  <label 
+                      for="${itemNameEncoded}Oversupply" 
+                      class="oversupply" 
+                      id="${itemNameEncoded}OversupplyLabel">
+                    Oversupply (too much)</label>
                 </div>
               </fieldset>
               <div class="horizontal update-confirm-div" id="${itemNameEncoded}UpdateConfirm">
@@ -126,6 +170,11 @@ function addItem() {
 
   document.getElementById("newItemText").value = "";
 
+  document.getElementById(`${itemNameEncoded}UpdateConfirm`).style.display = "block";
+  setTimeout(function () {
+        document.getElementById(`${itemNameEncoded}UpdateConfirm`).style.display = 'none';
+      },
+      1500);
 }
 
 
