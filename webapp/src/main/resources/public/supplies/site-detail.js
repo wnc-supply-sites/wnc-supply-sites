6d88138609f848d45dba64f685b08a9f335b24b4
DanVanAtta <dhvatta@gmail.com>
2024-11-19 10:19:08 -0500

    Fix for back button on site-details page

--- webapp/src/main/resources/public/supplies/site-detail.js
+++ webapp/src/main/resources/public/supplies/site-detail.js
@@ -1,7 +1,7 @@
 function goBack(){
   //if it was the first page
   if(history.length === 1){
-    window.location = "https://wnc-supplies.com/supplies"
+    window.location = "/supplies"
   } else {
     history.back();
   }
