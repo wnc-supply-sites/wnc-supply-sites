4ac108363dbc87332c42f5b442dd76ece5baa6d5
DanVanAtta <dhvatta@gmail.com>
2024-11-29 19:54:45 -0500

    Use @RestController over @Controller for webhook endpoint

--- webapp/src/main/java/com/vanatta/helene/supplies/database/dispatch/DispatchUpdatesWebhook.java
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/dispatch/DispatchUpdatesWebhook.java
@@ -2,14 +2,14 @@ package com.vanatta.helene.supplies.database.dispatch;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @Slf4j
-@Controller
+@RestController
 public class DispatchUpdatesWebhook {
 
   @PostMapping("/webhook/needs-request-update")
@@ -17,5 +17,4 @@ public class DispatchUpdatesWebhook {
     log.info("Webhook needs request update received data: {}", body);
     return ResponseEntity.ok("received");
   }
-
 }
