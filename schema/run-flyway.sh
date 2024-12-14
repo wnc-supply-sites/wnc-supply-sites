18ecad8acaefa125dae72ed886892c7b554972d3
DanVanAtta <dhvatta@gmail.com>
2024-12-14 18:18:38 -0500

    Patches for building flyway container

--- schema/run-flyway.sh
+++ schema/run-flyway.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-(
+
 set -eux
 
-cd "$(dirname "$0")"
+function runFlyway {
+  local dbName="$1"
+  docker run \
+    -e "FLYWAY_URL=jdbc:postgresql://host.docker.internal:5432/$dbName" \
+    -e FLYWAY_USER=postgres \
+    -e FLWAY_PASSWORD=blackbox \
+    local/flyway:latest \
+    -locations=filesystem:/flyway/sql \
+    -password=blackbox \
+    -validateMigrationNaming=true \
+    -connectRetries=2 \
+    -baselineOnMigrate=true \
+    repair migrate
+#    -skipExecutingMigrations=true \
+}
 
-docker build . --tag local/flyway:latest
-
-docker run \
-  -e FLYWAY_URL=jdbc:postgresql://host.docker.internal:5432/wnc_helene_test \
-  -e FLYWAY_USER=postgres \
-  -e FLWAY_PASSWORD=blackbox \
-  local/flyway:latest \
-  -locations=filesystem:/flyway/sql \
-  -password=blackbox \
-  -validateMigrationNaming=true \
-  -connectRetries=2 \
-  repair migrate
-  # -skipExecutingMigrations=true \
+(
+  cd "$(dirname "$0")"
+  docker build . --tag local/flyway:latest
+  runFlyway wnc_helene_test
+  runFlyway wnc_helene
 )
