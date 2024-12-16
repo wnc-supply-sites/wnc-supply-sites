5683f3e9fb9c541aeacda063b4ff00ac1e3bc771
DanVanAtta <dhvatta@gmail.com>
2024-12-16 02:16:32 -0500

    run-flyway: convenience flag to skip migrating test

--- schema/run-flyway.sh
+++ schema/run-flyway.sh
@@ -2,8 +2,22 @@
 
 set -eux
 
+# Set skipTestFlag to 'skipTest' if migrations have already been applied to test
+
+# EG: $0 skipTest
+
+skipTestFlag="${1-}"
+
+
+skipTest=false
+if [ "$skipTestFlag" == "skipTest" ]; then
+  skipTest=true
+fi
+
 function runFlyway {
   local dbName="$1"
+  local skip="${2-false}
+
   docker run \
     -e "FLYWAY_URL=jdbc:postgresql://host.docker.internal:5432/$dbName" \
     -e FLYWAY_USER=postgres \
@@ -14,13 +28,13 @@ function runFlyway {
     -validateMigrationNaming=true \
     -connectRetries=2 \
     -baselineOnMigrate=true \
+    -skipExecutingMigrations="$skip" \
     repair migrate
-#    -skipExecutingMigrations=true \
 }
 
 (
   cd "$(dirname "$0")"
   docker build . --tag local/flyway:latest
-  runFlyway wnc_helene_test
+  runFlyway wnc_helene_test "$skip"
   runFlyway wnc_helene
 )
