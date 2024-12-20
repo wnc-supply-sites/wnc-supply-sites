c23165a2cfbbb945165c89dc9d6d4e61095f9749
DanVanAtta <dhvatta@gmail.com>
2024-12-20 15:42:35 -0500

    Fix schema - fix up bad merge resolution

--- schema/V30__distance_matrix.sql
+++ schema/V30__distance_matrix.sql
@@ -2,9 +2,12 @@ create table site_distance_matrix(
   id serial primary key ,
   site1_id integer not null references site(id),
   site2_id integer not null references site(id),
-  distance integer,
-  drive_time integer
+  distance_miles decimal(1),
+  drive_time_seconds integer,
+  valid boolean
 );
+/** valid => null if we don't know. False if the google maps API can't compute a distance. True if we can compute a distance */
+
 
 alter table site_distance_matrix owner to wnc_helene;
 
@@ -13,4 +16,19 @@ alter table site_distance_matrix
 
 alter table site_distance_matrix
   add constraint site_distance_matrix_calculated check
-     ((distance is null and drive_time is null) or (distance is not null and drive_time is not null));
+     ((distance_miles is null and drive_time is null) or (distance_miles is not null and drive_time_seconds is not null));
+
+/**
+  If valid is null or false, then distance & drive time should be too. If valid is false.
+  Otherwise if valid is set, then we shoudl have distance and drive time computed
+ */
+alter table site_distance_matrix
+  add constraint site_distance_valid check
+    ((valid is null and distance_miles is null)
+       or (
+         valid = false and distance_miles is null) or (valid = true and distance_miles is not null));
+
+alter table site_distance_matrix
+  add constraint site_distance_distance_value check (distance_miles >= 0.0);
+alter table site_distance_matrix
+  add constraint site_distance_time_value check (drive_time_seconds >= 0.0);
