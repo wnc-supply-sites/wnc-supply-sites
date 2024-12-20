943bf8e68111415f16f0f8c6084a692f7d9f8859
DanVanAtta <dhvatta@gmail.com>
2024-12-19 20:45:12 -0500

    Update partners, add 'valley hope'

---
+++ webapp/src/test/java/com/vanatta/helene/supplies/database/driver/DriverControllerTest.java
@@ -0,0 +1,48 @@
+package com.vanatta.helene.supplies.database.driver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.vanatta.helene.supplies.database.TestConfiguration;
+import org.junit.jupiter.api.Test;
+
+
+class DriverControllerTest {
+
+  String exampleInput =
+      """
+     {
+        "airtableId":34,
+        "fullName":"Test Driver",
+        "email":"exampleEmail",
+        "phone":"(919) 111-1111",
+        "active":true,
+        "location":"nowheresville"
+      }
+  """;
+
+  @Test
+  void parseDriver() {
+    DriverController.DriverJson driver = DriverController.DriverJson.parseJson(exampleInput);
+    assertThat(driver.getAirtableId()).isEqualTo(34);
+    assertThat(driver.getFullName()).isEqualTo("Test Driver");
+    assertThat(driver.getEmail()).isEqualTo("exampleEmail");
+    assertThat(driver.getPhone()).isEqualTo("(919) 111-1111");
+    assertThat(driver.isActive()).isEqualTo(true);
+    assertThat(driver.getLocation()).isEqualTo("nowheresville");
+  }
+  
+  @Test
+  void upsert() {
+    DriverController driverController = new DriverController(TestConfiguration.jdbiTest);
+    driverController.receiveDriverUpdates(exampleInput);
+    
+    DriverDao.Driver driver = DriverDao.lookupByAirtableId(34L);
+    assertThat(driver.getFullName()).isEqualTo("Test Driver");
+    assertThat(driver.getEmail()).isEqualTo("exampleEmail");
+    assertThat(driver.getPhone()).isEqualTo("(919) 111-1111");
+    assertThat(driver.isActive()).isEqualTo(true);
+    assertThat(driver.getLocation()).isEqualTo("nowheresville");
+    
+  }
+  
+}
