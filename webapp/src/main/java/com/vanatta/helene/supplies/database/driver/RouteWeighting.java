ac5e9fa80cec63b118747ba5b36bd004a6d36fc4
DanVanAtta <dhvatta@gmail.com>
2024-12-22 18:12:54 -0500

    Work in progress - route weighting algorithm

---
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/driver/RouteWeighting.java
@@ -0,0 +1,81 @@
+package com.vanatta.helene.supplies.database.driver;
+
+public class RouteWeighting {
+
+
+  
+  /*
+  YES
+  -----
+  72 miles
+  Heaters   U
+  Kerosene  U
+
+
+
+  20.5 miles (26 min)
+  Childrens OTC Meds
+  Garbage Bags    U
+
+  100.0 miles
+  Heaters   U
+  Propane
+
+
+  120 miles
+  Heater
+ 
+  17.9 miles (22 min)
+  Water U
+
+
+  25.0 miles
+  Clothing (New)
+
+  1.0 miles (1 min)
+  n95 mask
+
+
+  49.5 miles (1 hr 13 min)
+  Motrin & Tylenol U
+
+
+
+  NO
+  ---
+
+  49.5 miles (1 hr 13 min)
+  Motrin & Tylenol
+
+
+  48.1 miles (1 hr 8 min)
+  Disinfectant
+  
+  30 miles
+  Clothing (New)
+  
+
+  70.9 miles (1 hr 30 min)
+  Dish Soap
+
+
+  30 miles
+  Christmas presents (For Teens)
+  Food (Non-Perishable)
+  
+  
+  200 miles
+  Heaters   U
+  Kerosene  U
+  
+  50 miles (26 min)
+  Childrens OTC Meds
+  Garbage Bags    U
+
+  
+
+  
+  
+   */
+
+}
