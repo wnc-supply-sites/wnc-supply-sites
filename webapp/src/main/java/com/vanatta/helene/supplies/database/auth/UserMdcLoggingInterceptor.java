f6ed3f477bb66768d57ebd2f3a0f9be9266b94f7
DanVanAtta <dhvatta@gmail.com>
2025-01-04 15:28:30 -0500

    Logging - fix MDC not being cleared, add date to logging, and add a request id to each request for logging

--- webapp/src/main/java/com/vanatta/helene/supplies/database/auth/UserMdcLoggingInterceptor.java
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/auth/UserMdcLoggingInterceptor.java
@@ -1,35 +1,35 @@
 package com.vanatta.helene.supplies.database.auth;
 
 import com.vanatta.helene.supplies.database.util.CookieUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
+import java.io.IOException;
+import java.util.UUID;
+
 import org.slf4j.MDC;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 /**
  * Reads 'user' cookie and puts the value into MDC. This allows any further logging messages to
  * automatically include this 'user' value.
  */
 @Controller
-public class UserMdcLoggingInterceptor implements WebMvcConfigurer {
+public class UserMdcLoggingInterceptor extends OncePerRequestFilter {
 
   @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(new UserLogging());
-  }
-
-  @AllArgsConstructor
-  static class UserLogging implements HandlerInterceptor {
-
-    @Override
-    public boolean preHandle(
-        HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-      CookieUtil.readUserCookie(request).ifPresent(user -> MDC.put("user", user));
-      return true;
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    CookieUtil.readUserCookie(request).ifPresent(user -> MDC.put("user", user));
+    MDC.put("requestId", UUID.randomUUID().toString().substring(0, 5));
+    try {
+      filterChain.doFilter(request, response);
+    } finally {
+      MDC.remove("user");
+      MDC.remove("requestId");
     }
   }
 }
