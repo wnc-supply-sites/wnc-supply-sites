37d46de833988a6953338828a59dbe01c15169aa
DanVanAtta <dhvatta@gmail.com>
2025-01-04 22:35:08 -0500

    Fix typo in webhook URL

--- webapp/src/main/java/com/vanatta/helene/supplies/database/incoming/webhook/InventoryWebhook.java
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/incoming/webhook/InventoryWebhook.java
@@ -1,17 +1,19 @@
-package com.vanatta.helene.supplies.database.supplies;
+package com.vanatta.helene.supplies.database.incoming.webhook;
 
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 /**
  * Webhook for receiving info about category information.
  */
 @Slf4j
 public class InventoryWebhook {
-
-  @PostMapping("/webook/inventory/update-category")
+  @PostMapping("/webhook/inventory/update-category")
   ResponseEntity<String> updateItemCategory(@RequestBody String input) {
     log.info("Received item category info: {}", input);
     
@@ -19,4 +21,9 @@ public class InventoryWebhook {
   }
   
 
+  @Value
+  static class ItemTagsInput {
+    long wssId;
+    List<String> descriptionTags;
+  }
 }
---
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/incoming/webhook/TODO.md
@@ -0,0 +1,5 @@
+Move the classes in this package to 'domain' specific packages.
+"incoming/webhooks" is generic, not good, does not describe
+"what" is in here - just describes how (which is not good!).
+
+EG: "needs.matching" package, etc...
