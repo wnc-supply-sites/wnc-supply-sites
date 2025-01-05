4ef4808b22509fde149e04cdebca7f6da819b15e
DanVanAtta <dhvatta@gmail.com>
2025-01-04 23:52:15 -0500

    Add missing @Controller to webhook controller

--- webapp/src/main/java/com/vanatta/helene/supplies/database/incoming/webhook/InventoryWebhook.java
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/incoming/webhook/InventoryWebhook.java
@@ -3,6 +3,7 @@ package com.vanatta.helene.supplies.database.incoming.webhook;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,7 +13,9 @@ import java.util.List;
  * Webhook for receiving info about category information.
  */
 @Slf4j
+@Controller
 public class InventoryWebhook {
+
   @PostMapping("/webhook/inventory/update-category")
   ResponseEntity<String> updateItemCategory(@RequestBody String input) {
     log.info("Received item category info: {}", input);
