39de3205db35705b78a14fd4de199a4251a7dc63
DanVanAtta <dhvatta@gmail.com>
2025-01-05 19:34:31 -0500

    Stop logging  sensitive ENV variables on startup

--- webapp/src/main/java/com/vanatta/helene/supplies/database/EnvLoggingConfiguration.java
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/EnvLoggingConfiguration.java
@@ -14,37 +14,21 @@ public class EnvLoggingConfiguration {
       @Value("${jdbi.user}") String user,
       @Value("${auth.enabled}") boolean authEnabled,
       @Value("${make.enabled}") boolean makeEnabled,
-      @Value("${make.webhook.upsertSite}") String upsertWebhook,
-      @Value("${make.webhook.newItem}") String newItemWebhook,
-      @Value("${make.webhook.updateInventory}") String updateInventoryWebhook,
-      @Value("${airtable.webhook.delivery.update}") String updateDeliveryWebhook,
-      @Value("${airtable.webhook.driver.update}") String updateDriverWebhook,
-      @Value("${airtable.webhook.item.merge}") String itemMergeWebhook,
-      @Value("${make.webhook.dispatch.new}") String dispatchWebhook,
       @Value("${distance.calculator.enabled}") boolean distanceCalculatorEnabled,
       @Value("${distance.calculator.delay.ms}") int distanceCalculatorDelayMs,
       @Value("${google.maps.api.key}") String googleApiKey,
       @Value("${twilio.from.number}") String twilioFromNumber,
       @Value("${twilio.account.sid}") String twilioAccountSid,
-      @Value("${twilio.auth.token}") String twilioAuthToken,
       @Value("${twilio.sms.enabled}") boolean twilioSmsEnabled) {
     log.info("ENV - JDBI URL: {}", url);
     log.info("ENV - JDBI USER: {}", user);
     log.info("ENV - AUTH ENABLED: {}", authEnabled);
     log.info("ENV - MAKE ENABLED: {}", makeEnabled);
-    log.info("ENV - UPSERT WEBHOOK: {}", upsertWebhook);
-    log.info("ENV - NEW ITEM WEBHOOK: {}", newItemWebhook);
-    log.info("ENV - UPDATE INVENTORY WEBHOOK: {}", updateInventoryWebhook);
-    log.info("ENV - UPDATE DELIVERY WEBHOOK: {}", updateDeliveryWebhook);
-    log.info("ENV - UPDATE DRIVER WEBHOOK: {}", updateDeliveryWebhook);
-    log.info("ENV - ITEM MERGE WEBHOOK: {}", itemMergeWebhook);
-    log.info("ENV - DISPATCH WEBHOOK: {}", dispatchWebhook);
     log.info("ENV - DISTANCE CALCULATOR ENABLED: {}", distanceCalculatorEnabled);
     log.info("ENV - DISTANCE CALCULATOR DELAY: {}", distanceCalculatorDelayMs);
     log.info("ENV - GOOGLE API KEY: {}", googleApiKey);
     log.info("ENV - TWILIO FROM NUMBER: {}", twilioFromNumber);
     log.info("ENV - TWILIO ACCOUNT SID: {}", twilioAccountSid);
-    log.info("ENV - TWILIO AUTH TOKEN: {}", twilioAuthToken);
     log.info("ENV - TWILIO SMS ENABLED: {}", twilioSmsEnabled);
   }
 }
