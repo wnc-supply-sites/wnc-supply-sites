95e376a9eaf28b737d5de4f9139c0007640ec071
DanVanAtta <dhvatta@gmail.com>
2025-01-11 14:14:51 -0500

    Remove debug logging of headers

--- webapp/src/main/java/com/vanatta/helene/supplies/database/DeploymentAdvice.java
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/DeploymentAdvice.java
@@ -5,20 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.util.List;
-
 /**
- * Configures parameters based upon the requested domain. Different deployments are partitioned
- * by the domain name.
+ * Configures parameters based upon the requested domain. Different deployments are partitioned by
+ * the domain name.
  */
-
 @ControllerAdvice
 @Slf4j
 public class DeploymentAdvice {
   @ModelAttribute("domain")
   public String domain(HttpServletRequest request) {
-    log.info("All headers: {}", List.of(request.getHeaderNames().asIterator()));
-    log.info("Host header: {}", request.getHeader("host"));
+    //    log.info("Host header: {}", request.getHeader("host"));
     return "";
   }
 }
