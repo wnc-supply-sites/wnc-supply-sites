4de96e5c2f5f26f577f96b7ec1bee011056db34a
Colormethanh <thnguyen1847@gmail.com>
2025-01-10 19:00:36 -0500

    added scroll to top button on inventory management

--- webapp/src/main/resources/public/manage/inventory/inventory.js
+++ webapp/src/main/resources/public/manage/inventory/inventory.js
@@ -520,3 +520,36 @@ function formatTagElementValue(tag){
   return tag;
 }
 
+/**
+ * Scroll to top functionality
+ */
+
+function instantiateScrollToTopEvenListener() {
+  const scrollToTopButton = document.getElementsByClassName("scroll-to-top-btn")[0];
+
+  scrollToTopButton.addEventListener("click", () => {
+    window.scrollTo({top: 0, left: 0, behavior: "smooth"});
+  });
+
+  scrollToTopButton.addEventListener("mouseover", () => {
+    scrollToTopButton.classList.add("opacity-full");
+  })
+
+  scrollToTopButton.addEventListener("mouseout", () => {
+    scrollToTopButton.classList.remove("opacity-full");
+  })
+
+  window.addEventListener("scroll", (e) => {
+    const scrollTop = document.body.scrollTop
+    console.log(scrollTop);
+    console.log(`window height: ${window.innerHeight}`)
+
+    if (scrollTop > .5 * window.innerHeight) {
+      scrollToTopButton.classList.remove("hidden")
+    } else {
+      scrollToTopButton.classList.add("hidden")
+    }
+    
+  })
+}
+
