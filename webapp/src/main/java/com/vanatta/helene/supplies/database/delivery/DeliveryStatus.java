1a7941c1b4daa548a03c0c4dd79a0926ecfc2c68
DanVanAtta <dhvatta@gmail.com>
2025-01-11 21:01:16 -0500

    Add 'Driver Volunteered' delivery status

--- webapp/src/main/java/com/vanatta/helene/supplies/database/delivery/DeliveryStatus.java
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/delivery/DeliveryStatus.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum DeliveryStatus {
+  DRIVER_VOLUNTEERED("Driver Volunteered"),
   CREATING_DISPATCH("Creating Dispatch"),
   ASSIGNING_DRIVER("Assigning Driver"),
   CONFIRMING("Confirming"),
