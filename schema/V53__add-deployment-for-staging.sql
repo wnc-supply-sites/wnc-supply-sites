c280c952e1c6f64a2f1851783abd9d802f1292dd
DanVanAtta <dhvatta@gmail.com>
2025-01-12 17:08:25 -0500

    Add deployment mapping for 'staging.wnc-supply-sites.com'

---
+++ schema/V53__add-deployment-for-staging.sql
@@ -0,0 +1,7 @@
+insert into deployment(domain, short_name, contact_us_link, site_description)
+values(
+       'staging.wnc-supply-sites.com',
+       'Staging',
+       'https://form.jotform.com/243608573773062',
+       'WSS Staging'
+    );
