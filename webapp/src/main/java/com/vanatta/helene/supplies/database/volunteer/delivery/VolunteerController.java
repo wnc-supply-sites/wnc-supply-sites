7aecaaa10872daf5e864811d06f1a4dee3052b4d
Colormethanh <thnguyen1847@gmail.com>
2025-03-10 23:33:46 -0400

    added states list to page param

--- webapp/src/main/java/com/vanatta/helene/supplies/database/volunteer/delivery/VolunteerController.java
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/volunteer/delivery/VolunteerController.java
@@ -1,23 +1,39 @@
 package com.vanatta.helene.supplies.database.volunteer.delivery;
 
+import com.vanatta.helene.supplies.database.DeploymentAdvice;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import org.jdbi.v3.core.Jdbi;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 @AllArgsConstructor
 public class VolunteerController {
+  private final Jdbi jdbi;
 
+  /** Users will be shown a form to request to make a delivery */
   @GetMapping("/volunteer/delivery")
-  public ModelAndView deliveryForm(
-      HttpServletRequest request
+  ModelAndView deliveryForm(
+      @ModelAttribute(DeploymentAdvice.DEPLOYMENT_STATE_LIST) List<String> states
   ) {
-    Map<String, Object> params = new HashMap<>();
-    return new ModelAndView("volunteer/delivery/deliveryForm", params);
+    return deliveryForm(jdbi, states);
   }
+
+  public static ModelAndView deliveryForm(Jdbi jdbi, List<String> states) {
+    Map<String, Object> pageParams = new HashMap<>();
+    pageParams.put("state", states);
+    return new ModelAndView("volunteer/delivery/deliveryForm", pageParams);
+  }
+
+
+
+
+
 }
