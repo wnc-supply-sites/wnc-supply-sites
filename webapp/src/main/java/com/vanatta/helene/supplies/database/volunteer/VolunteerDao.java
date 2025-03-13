10f0df1b19b9e7643eb6639730dba02df27d1b7a
Colormethanh <thnguyen1847@gmail.com>
2025-03-13 13:57:53 -0400

    added DAO query to get site needed items and site info

--- webapp/src/main/java/com/vanatta/helene/supplies/database/volunteer/VolunteerDao.java
+++ webapp/src/main/java/com/vanatta/helene/supplies/database/volunteer/VolunteerDao.java
@@ -2,7 +2,9 @@ package com.vanatta.helene.supplies.database.volunteer;
 
 import lombok.extern.slf4j.Slf4j;
 import org.jdbi.v3.core.Jdbi;
+import com.vanatta.helene.supplies.database.volunteer.VolunteerController.SiteSelect;
 import com.vanatta.helene.supplies.database.volunteer.VolunteerController.Site;
+import com.vanatta.helene.supplies.database.volunteer.VolunteerController.Item;
 
 import java.util.List;
 
@@ -10,13 +12,13 @@ import java.util.List;
 public class VolunteerDao {
 
 
-  static List<Site> fetchSites(Jdbi jdbi, List<String>states) {
+  static List<SiteSelect> fetchSiteSelect(Jdbi jdbi, List<String>states) {
     return jdbi.withHandle(
         handle ->
             handle
                 .createQuery(
             """
-                select s.id, s.name, c.name as countyName, c.state
+                select s.id, s.name, c.name as county, c.state
                 from site s
                 join county c on c.id = s.county_id
                 where
@@ -28,8 +30,48 @@ public class VolunteerDao {
                 order by lower(s.name)
                 """)
                 .bindList("states", states)
-                .mapToBean(Site.class)
+                .mapToBean(SiteSelect.class)
                 .list());
+  }
+
+
+  static Site fetchSiteItems(Jdbi jdbi, Long siteId) {
+    return  jdbi.withHandle(handle -> {
+          Site site = handle.createQuery("""
+                select s.id, s.name, c.name as county, c.state
+                from site s
+                join county c on c.id = s.county_id
+                where
+                s.id = :siteId
+                and 
+                s.active
+                and 
+                s.publicly_visible
+              """)
+              .bind("siteId", siteId)
+              .mapToBean(Site.class)
+              .findOne()
+              .orElse(null);
+
+          if (site == null) {
+            return null;
+          };
+
+          List<Item> items = handle.createQuery("""
+              select si.id as id, i.name as name, ist.name as status
+              from site_item si
+              join item i on si.item_id = i.id
+              join item_status ist on si.item_status_id = ist.id
+              where si.site_id = :siteId
+              and ist.is_need
+              order by ist.sort_order, i.name
+              """)
+              .bind("siteId", siteId)
+              .mapToBean(VolunteerController.Item.class)
+              .list();
+          site.setItems(items);
+          return site;
+    });
   }
 
 }
--- webapp/src/main/resources/public/volunteer/delivery-form.js
+++ webapp/src/main/resources/public/volunteer/delivery-form.js
@@ -1,16 +1,19 @@
+
+
 async function fetchSiteData (siteId) {
     const response = await fetch(`/volunteer/site-items?siteId=${siteId}`);
+    console.log(await response.json());
 }
 
 function instantiateEventListeners() {
-
     const siteSelect = document.getElementById("site-select");
     siteSelect.addEventListener("change", (e) => {
+        console.log("Site changed :", e.target.value);
+        fetchSiteData(e.target.value);
     });
 
-
-
 }
+
 
 window.addEventListener("load", () => {
     instantiateEventListeners();
