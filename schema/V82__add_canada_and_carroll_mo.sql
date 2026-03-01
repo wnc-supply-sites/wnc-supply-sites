7e403e03b1fbf902205497481e7f9cc38566b64b
Kayla <imbrialk@bc.edu>
2026-03-01 17:29:29 -0500

    Add Canada and Carroll County, MO

---
+++ schema/V82__add_canada_and_carroll_mo.sql
@@ -0,0 +1,17 @@
+-- Add Canada provinces & territories
+insert into county(name, state) values('Alberta', 'Canada');
+insert into county(name, state) values('British Columbia', 'Canada');
+insert into county(name, state) values('Manitoba', 'Canada');
+insert into county(name, state) values('New Brunswick', 'Canada');
+insert into county(name, state) values('Newfoundland and Labrador', 'Canada');
+insert into county(name, state) values('Nova Scotia', 'Canada');
+insert into county(name, state) values('Ontario', 'Canada');
+insert into county(name, state) values('Prince Edward Island', 'Canada');
+insert into county(name, state) values('Quebec', 'Canada');
+insert into county(name, state) values('Saskatchewan', 'Canada');
+insert into county(name, state) values('Northwest Territories', 'Canada');
+insert into county(name, state) values('Nunavut', 'Canada');
+insert into county(name, state) values('Yukon', 'Canada');
+
+-- Add missing Carroll County, Missouri
+insert into county(name, state) values('Carroll', 'MO');
